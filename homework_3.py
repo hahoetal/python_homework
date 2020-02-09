9bc4015dda233321ee3f660aec6238ea73ee5797
hahoetal <49137087+hahoetal@users.noreply.github.com>
2020-02-09 14:08:43 +0900

    Update homework_3.py
    파이썬을 다시 공부하면서 작성.

--- homework_3.py
+++ homework_3.py
@@ -5,3 +5,12 @@ a = len(word)
 for i in range(1,a+1):
     print(word[a-i], end='')
     
+-------------------------------------------------------
+word = input('단어를 입력해주세요 : ')
+
+# for 변수 in 시퀀스 객체(리스트, 튜플, 문자열, range)
+# reversed(시퀀스 객체) 시퀀스 객체를 뒤집어줌
+
+for w in reversed(word):  
+    print(w, end = '')
+# word에 저장된 문자열에서 요소(문자)를 하나씩 꺼내서 w에 저장하는 것을 word에 있는 모든 요소를 꺼낼 때까지 반복
